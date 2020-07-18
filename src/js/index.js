9c2588af2f53d2f5c125920a434afc59f2bc6dc3
marlucz <marcin_luczak@hotmail.com>
2020-07-18 22:49:00 +0200

    validate inputs - custom fix for select inputs

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,24 @@ import * as mdb from 'mdb-ui-kit';
 export default {
   mdb,
 };
+
+window.addEventListener('load', () => {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+});
